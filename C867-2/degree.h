67048976e8cb749993b44909cdc0b58bf27f5d74
Jacob Petrakovitz <japesc20@gmail.com>
2023-12-07 19:35:25 -0500

    Add project files.

---
+++ C867-2/degree.h
@@ -0,0 +1,7 @@
+#pragma once
+
+enum {
+	SECURITY, 
+	NETWORK, 
+	SOFTWARE
+};
---
+++ C867-2/student.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+
+
+class Student{
+	
+	public:
+		std::string studentID;
+		std::string firstName;
+		std::string lastName;
+		std::string emailAddress;
+		int age;
+		int daysToCompletion[3];
+		std::string degreeProgram;
+
+};
